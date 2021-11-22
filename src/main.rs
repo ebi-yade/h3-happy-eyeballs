0bd142667ca99d03f295b607f88d89d1f37ba66e
市川恭佑 (ebi) <github@mail.ebi-yade.com>
2021-11-22 17:10:38 +0900

    Initialize command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+  let mut args = std::env::args();
+  let cmd = &args.next().unwrap();
+  if args.len() != 1 {
+      println!("Usage : {} URL", cmd);
+      println!("invalid number of argument");
+      println!("expected {}, but you passed {}", 1, args.len());
+      return;
+  }
+
+  let url = url::Url::parse(&args.next().unwrap()).unwrap();
+  println!("input url: {}", url);
 }
